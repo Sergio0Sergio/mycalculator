6b220d186b3db1289e029a579f1df25ecc57878d
Sergei Grimanov <tosergi@gmail.com>
2016-09-11 22:58:56 +0300

    добавлены методы: operandInput calculate

--- mycalculator/src/main/java/ru/habrahabr/sergiosergio/mycalculator/CalcEngine.java
+++ mycalculator/src/main/java/ru/habrahabr/sergiosergio/mycalculator/CalcEngine.java
@@ -1,14 +1,107 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Класс является движком калькулятора, те
+ * содержит набор методов, в которые передаются
+ * операторы и операнды в формате char, при
+ * этом возвращается содержимое индикатора калькулятора
+ * в формате String.
+ * 
  */
 package ru.habrahabr.sergiosergio.mycalculator;
 
 /**
  *
  * @author Sergei Grimanov
+ * @version 1.0
  */
 public class CalcEngine {
+    
+   private StringBuilder display = new StringBuilder("0");
+   private char operator;
+   private double operandA;
+   //private double operandB;
+   private double result;
+   private boolean displayFlag = false;
+   private boolean operandAFlag = false;
+   
+   /**
+    * Принимает число, возвращает содержимое дисплея калькулятора.
+    * Является частью API.
+    * @param c передаваемое для вычислений число (кроме дробной точки).
+    * @return 
+    */
+   public String numberInput(char c){
+       
+       if (displayFlag == false){
+          
+           display.delete(0, display.length());
+           display.append('0');
+       }
+       
+       if (display.toString().equals("0")){
+           
+           display.deleteCharAt(0);
+       }
+       
+       display.append(c);
+       return display.toString();
+   }
+   
+   /**
+    * Принимает оператор (+,-,*,/) и возвращает содержимое 
+    * дисплея калькулятора. Является частью API. 
+    * @param c передаваемый оператор для вычислений (+,-,*,/)
+    * @return 
+    */
+   public String operatorInput(char c){
+       
+       if(operandAFlag == true){
+           
+           if(displayFlag == true){
+               
+               result = calculateResult(c);
+               operandA = result;
+               display.delete(0, display.length());
+               display.append(String.valueOf(result));
+               displayFlag = false;
+               operator = c;
+            }
+           
+           else{
+               
+               operator = c;
+           }
+       }
+       
+       else{
+           
+           operator = c;
+           operandA = Double.parseDouble(display.toString());
+           operandAFlag = true;
+           displayFlag = false;
+       }
+       return display.toString();
+    
+       
+   }
+   
+   private double calculateResult(char c){
+       
+       switch (c){
+           case '+': 
+               return operandA + Double.parseDouble(display.toString());
+           case '-': 
+               return operandA - Double.parseDouble(display.toString());
+           case '*': 
+               return operandA * Double.parseDouble(display.toString());
+           case '/': 
+               if (operandA == 0){
+                   return 0;
+               }
+                   
+               else {return operandA / Double.parseDouble(display.toString());}
+            
+           default: return 0;
+       }
+   }
     
 }
