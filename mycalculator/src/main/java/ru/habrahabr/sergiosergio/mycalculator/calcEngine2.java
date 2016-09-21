cafa42115efae2ee5ca25976b097f36364cac3ae
Sergei Grimanov <tosergi@gmail.com>
2016-09-21 21:04:42 +0300

    Создан новый класс calcEngine2.java. В нем реализован метод обработки поступивших чисел.

---
+++ mycalculator/src/main/java/ru/habrahabr/sergiosergio/mycalculator/calcEngine2.java
@@ -0,0 +1,64 @@
+package ru.habrahabr.sergiosergio.mycalculator;
+
+public class calcEngine2 {
+	
+	private char lastSymbol = '0';
+	private char operator;
+	private double operandA;
+	private int state = 1;
+	private StringBuilder display = new StringBuilder("0");
+	
+	public String numberInput (char c){
+		
+		switch (state){
+		
+			case 0:
+								
+				checkZero();
+				display.append(c);
+				lastSymbol = 'c';
+				return  display.toString();
+								
+			case 1:
+				
+				checkLastSymbol();
+				checkZero();
+				display.append('c');
+				lastSymbol = 'c';
+				return  display.toString();
+				
+			case 2:
+				
+				state = 2;
+				display.delete(0, display.length());
+				display.append(c);
+				return  display.toString();
+				
+				
+		}
+		return null;
+		
+		
+		
+		
+	}
+	
+	private void checkZero(){
+		
+		if(display.toString().equals("0")){
+			
+			display.deleteCharAt(0);
+		}
+		
+	}
+	
+	private void checkLastSymbol(){
+		
+		if (lastSymbol == '+' || lastSymbol == '-' || lastSymbol == '*' || lastSymbol == '/' || lastSymbol == '='){
+			
+			display.delete(0, display.length());
+			display.append("0");
+		}
+	}
+
+}
